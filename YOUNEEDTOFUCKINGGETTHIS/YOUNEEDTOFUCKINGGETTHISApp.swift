772d5019223bff247f8d711f1ac599136c89d91c
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-05-31 13:25:04 -0400

    Initial Commit

---
+++ YOUNEEDTOFUCKINGGETTHIS/YOUNEEDTOFUCKINGGETTHISApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOUNEEDTOFUCKINGGETTHISApp.swift
+//  YOUNEEDTOFUCKINGGETTHIS
+//
+//  Created by Kevin Buchholz on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct YOUNEEDTOFUCKINGGETTHISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
